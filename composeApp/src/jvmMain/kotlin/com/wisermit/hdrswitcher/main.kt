69c28137e4d75384298ef500bcc442bc89aa48a3
Marco Kosan <marco.kosan42@gmail.com>
2025-09-23 19:07:13 -0300

    update log level

--- composeApp/src/jvmMain/kotlin/com/wisermit/hdrswitcher/main.kt
+++ composeApp/src/jvmMain/kotlin/com/wisermit/hdrswitcher/main.kt
@@ -10,6 +10,7 @@ import androidx.compose.ui.unit.DpSize
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.application
 import com.wisermit.hdrswitcher.di.AppModule
+import com.wisermit.hdrswitcher.framework.Log
 import com.wisermit.hdrswitcher.ui.FluentWindow
 import com.wisermit.hdrswitcher.ui.SystemTray
 import com.wisermit.hdrswitcher.ui.main.MainScreen
@@ -31,6 +32,8 @@ private val WINDOW_SIZE = DpSize(
 )
 
 fun main() = application {
+    Log.level = Log.Level.Test
+
     KoinApplication(
         application = {
             modules(AppModule.modules)
